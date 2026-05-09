5ec03bbaafb01a5b554e4d2fc130a6c44f86e37d
bu1ycheva <mashabulycheva07@gmail.com>
2026-05-09 18:11:56 +0700

    feat(game): integrate mapgen with level initialization and platforms visualization

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,13 +2,19 @@ extends Node2D
 
 enum Scene { MAIN_MENU, MODE_SELECT, MAP_SELECT, GAME }
 var scene : int = Scene.MAIN_MENU
-
 var main_menu_cursor : int = 0
 var mode_cursor      : int = 0
 var map_cursor       : int = 11
 
 var scores       : Array = [0, 0]
 var player_count : int = 1
+
+var platforms : Array = []
+var map_bg    : int = 0
+
+var lava_y     : float = 270.0
+var lava_speed : float = 0.0
+
 
 func _process(delta: float) -> void:
 	if has_node("/root/Renderer"):
@@ -47,7 +53,7 @@ func _input(event: InputEvent) -> void:
 			if map_cursor > 0: map_cursor -= 1
 			
 		if event.is_action_pressed("ui_accept"):
-			scene = Scene.GAME
+			_start_match()
 		elif event.is_action_pressed("ui_cancel"):
 			scene = Scene.MODE_SELECT
 
@@ -66,3 +72,35 @@ func _draw() -> void:
 			
 		Scene.GAME:
 			draw_string(ThemeDB.fallback_font, Vector2(150, 130), "ТУТ БУДЕТ ИГРА", HORIZONTAL_ALIGNMENT_LEFT, -1, 10)
+			for plat in platforms:
+				Renderer.draw_platform(self, plat, map_bg, Renderer.time)
+
+
+func _start_match() -> void:
+	var map_data : Dictionary = Mapgen.generate(Globals.selected_mode)
+	
+	map_bg = map_data["bg"]
+	platforms.clear()
+	
+	var raw_plats : Array = map_data.get("plats", map_data.get("crumble_tiles", []))
+	
+	for p in raw_plats:
+		var cx: float = p[0]
+		var cy: float = p[1]
+		var w: float = p[2]
+		var h: float = p[3]
+		
+		var rect := Rect2(cx - w/2.0, cy - h/2.0, w, h)
+		
+		platforms.append({
+			"rect": rect,
+			"alive": true,
+			"shake": 0.0,
+			"marked": false
+		})
+	
+	if Globals.selected_mode == 2:
+		lava_y = map_data.get("ls", 250.0)
+		lava_speed = map_data.get("lspd", 5.0)
+		
+	scene = Scene.GAME
